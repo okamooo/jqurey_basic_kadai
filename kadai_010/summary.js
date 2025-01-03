0bfc80e88c8dea11a35c13d5c68acb90789176c2
okamooo <okamo0180@gmail.com>
2025-01-03 18:48:47 +0900

    課題10をアップロード

---
+++ kadai_010/summary.js
@@ -0,0 +1,15 @@
+$(function() {   
+     // ボタンがクリックされた場合   
+      $('#change-color').on('click', function(){     
+         $('#target').css('color', 'red'); 
+           });     
+      $('#change-text').on('click', function(){     
+         $('#target').text('hello,World!');  
+          });    
+      $('#fade-out').on('click', function(){     
+         $('#target').fadeOut();  
+          });        
+      $('#fade-in').on('click', function(){     
+          $('#target').fadeIn();  
+          }); 
+        });
